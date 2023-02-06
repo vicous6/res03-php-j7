09a1a271d3554f57bb08da382c18bc21410596ca
vicous6 <vicous@free.fr>
2023-02-06 16:24:00 +0000

    commit

--- exercice/pages/account.php
+++ exercice/pages/account.php
@@ -1,6 +1,12 @@
 <?php 
+if($_SESSION["status"]===true){
+    $template = "account";
+    require "templates/layout.phtml";
+}else{
+    
+    require "homepage.php";
+}
 
-$template = "account";
 
-require "templates/layout.phtml"
+
 ?>
